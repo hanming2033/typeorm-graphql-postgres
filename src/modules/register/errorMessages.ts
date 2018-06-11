8c8b3edfd002b198fcf9a0d5d3f991597ecbaac5
hanming <hanming2033@gmail.com>
2018-06-11 23:10:17 +0800

    refined test with jest

--- src/modules/register/errorMessages.ts
+++ src/modules/register/errorMessages.ts
@@ -1,3 +1,4 @@
 export const EMAIL_DUPLICATE = 'Email Already Taken'
 export const EMAIL_TOO_SHORT = 'email must be at least 3 characters'
 export const EMAIL_INVALID = 'email must be a valid email'
+export const PASSWORD_TOO_SHORT = 'password must be at least 3 characters'
--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,7 +1,7 @@
 import { request } from 'graphql-request'
 import { User } from '../../entity/User'
 import { startServer } from '../../startServer'
-import { EMAIL_DUPLICATE, EMAIL_INVALID, EMAIL_TOO_SHORT } from './errorMessages';
+import { EMAIL_DUPLICATE, EMAIL_INVALID, EMAIL_TOO_SHORT, PASSWORD_TOO_SHORT } from './errorMessages'
 
 let host = ''
 
@@ -25,34 +25,49 @@ mutation {
 `
 
 // this test requires the server to start and run in the background
-test('Register User', async () => {
-  // *test normal registering user
-  // send register mutation
-  const response1 = await request(host, mutation(email, password))
-  // check if register null ==> successful
-  expect(response1).toEqual({ register: null })
-  // try to find all users with the email
-  const users = await User.find({ where: { email } })
-  // check if users return only have 1 user
-  expect(users).toHaveLength(1)
-  // check if user returned is same as the user we created
-  const user = users[0]
-  expect(user.email).toEqual(email)
-  expect(user.password).not.toEqual(password)
+describe('Registering User', async () => {
+  it('tests normal registering user', async () => {
+    // send register mutation
+    const response1 = await request(host, mutation(email, password))
+    // check if register null ==> successful
+    expect(response1).toEqual({ register: null })
+    // try to find all users with the email
+    const users = await User.find({ where: { email } })
+    // check if users return only have 1 user
+    expect(users).toHaveLength(1)
+    // check if user returned is same as the user we created
+    const user = users[0]
+    expect(user.email).toEqual(email)
+    expect(user.password).not.toEqual(password)
+  })
 
-  // *test depulicate user registeration
-  // error ==> {register:[{path:'email', message:'...'},{}]}
-  const response2: any = await request(host, mutation(email, password))
-  // check if mutation return exactly 1 object
-  expect(response2.register).toHaveLength(1)
-  // check if the first error's path is email
-  expect(response2.register[0]).toEqual({ path: 'email', message: EMAIL_DUPLICATE })
+  it('tests for duplicated registering', async () => {
+    // error ==> {register:[{path:'email', message:'...'},{}]}
+    const response2: any = await request(host, mutation(email, password))
+    // check if mutation return exactly 1 object
+    expect(response2.register).toHaveLength(1)
+    // check if the first error's path is email
+    expect(response2.register[0]).toEqual({ path: 'email', message: EMAIL_DUPLICATE })
+  })
 
-  // *test bad email
-  // error ==> {register:[{path:'email', message:'...'},{}]}
-  const response3: any = await request(host, mutation('b', password))
-  // above email will fail with 2 errors
-  expect(response3).toEqual({ register: [{ path: 'email', message: EMAIL_TOO_SHORT  }, { path: 'email', message: EMAIL_INVALID }] })
+  it('tests for bad email', async () => {
+    // error ==> {register:[{path:'email', message:'...'},{}]}
+    const response3: any = await request(host, mutation('b', password))
+    // above email will fail with 2 errors
+    expect(response3).toEqual({ register: [{ path: 'email', message: EMAIL_TOO_SHORT }, { path: 'email', message: EMAIL_INVALID }] })
+  })
 
-  // TODO: https://www.youtube.com/watch?v=JMLTlMAejX4 16:30
+  it('tests bad password', async () => {
+    // error ==> {register:[{path:'email', message:'...'},{}]}
+    const response4: any = await request(host, mutation('bf', 'ad'))
+    // above email will fail with 2 errors
+    expect(response4).toEqual({
+      register: [
+        { path: 'email', message: EMAIL_TOO_SHORT },
+        { path: 'email', message: EMAIL_INVALID },
+        { path: 'password', message: PASSWORD_TOO_SHORT }
+      ]
+    })
+  })
 })
+// TODO: https://www.youtube.com/watch?v=JMLTlMAejX4 16:30
